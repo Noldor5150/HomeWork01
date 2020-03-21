7f20ed7155c49580b0091b20650b4161161213d9
Noldor5150 <ruikis.paulius@gmail.com>
2020-03-21 14:04:45 +0200

    function  numberInRange created

--- HomeWork01/Program.cs
+++ HomeWork01/Program.cs
@@ -8,45 +8,15 @@ namespace HomeWork01
     {
         static void Main(string[] args)
         {
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numberInRange(input,-9,9));
             
-            string digit = Console.ReadLine();
-            Console.WriteLine(digit);
-            Console.WriteLine(stringIsANumber(digit));
         }
 
-        static bool stringIsANumber(string possibleNumber)
+        static bool numberInRange(int inputValue,int bottomLimit, int upperLimit)
         {
-            bool stringIsANumber = false;
-            char[] rightNumbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            char[] arrayOfPossibleNumbers = possibleNumber.ToCharArray();
-            List<char> listOfPossibleNumbers = arrayOfPossibleNumbers.ToList();
-            List<char> compareChars = new List<char>() { };
-
-            if (listOfPossibleNumbers[0] == '-')
-            {
-                listOfPossibleNumbers.RemoveAt(0);
-                char[] arrayPositivePossibleNumber = listOfPossibleNumbers.ToArray();
-                possibleNumber = new string(arrayPositivePossibleNumber);
-            }
-                foreach (char posibleNumber in listOfPossibleNumbers)
-                {
-                    foreach (char rightNumber in rightNumbers)
-                    {
-                        if (posibleNumber == rightNumber)
-                        {
-                            compareChars.Add(posibleNumber);
-                        }
-                    }
-                }
-                char[] array = compareChars.ToArray();
-                string myNumber = new string(array);
-
-                if (myNumber == possibleNumber)
-                {
-                    stringIsANumber = true;
-                }
-            
-            return stringIsANumber;
+            return inputValue <= upperLimit && inputValue >= bottomLimit;
         }
     }
 }
